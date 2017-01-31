a3233a6cc5d602fca63b8c142c96fc069c649dca
Arjun Kalburgi <askalburgi@gmail.com>
2017-01-31 15:35:52 -0700

    more organization and figuring out

--- findpattern.c
+++ findpattern.c
@@ -2,30 +2,50 @@
 #include <stdlib.h>
 
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength, struct patmatch *locations, unsigned int loclength) {
-	// get access to memory (system call) 
-	start; 
-	end; 
+	// get access to memory (system call?) 
+	unsigned int start = 0; 
+	unsigned int end = 2^32; 
 
 	unsigned int patsfound = 0; 
 
-	for (var memory=start; memory<=end; memory++) {
-		for (var pattern=0; pattern<=patlength; pattern++) {
-			if (&pattern == &memory) {
-				if (pattern == patlength) {
-					if (patsfound < loclength) {
-						// make patmatch 
-						// add patmatch to locations	
+	for (int memory_index=start; memory_index<=end; memory_index++) {
+		for (int pattern_index=0; pattern_index<=patlength; pattern_index++) {
+
+			if (&pattern_index == &memory_index) { // the item at pattern_index is the same as the item at memory_index
+
+				if (pattern_index == patlength) { // we are at the last item in the pattern
+					if (patsfound < loclength) { // we are under the storage limit
+						patternFound(memory_index, locations); // make and store
 					}
-					patsfound++; 
+					patsfound++; // increment counter
+				} else { // we are not at the last item in the pattern
+					continue; // check the next one
 				}
-				continue; 
-			} elseif (&pattern != &memory) {
-				memory = memory - pattern + 1; 
-				break; 
+
+			} elseif (&pattern_index != &memory_index) { // we're not at a pattern
+				memory_index = memory_index - pattern_index + 1; // reset memory_index
+				break; // start over
 			}
-			memory++;
+			memory_index++;
 		}
 	}
 
-	return locations
+	return patsfound;
+}
+
+void patternFound(int memory_index, struct patmatch *locations) {
+	struct patmatch temp; 
+	// make patmatch 
+	strcpy (temp.location, memory_index); 
+	strcpy (temp.mode, memType(memory_index));
+	
+	locations[memory_index] = temp; 
+}
+
+int isReadOnly(int memory_index) {
+	if (MEM[memory_index].RW) {
+		return MEM_RW; 
+	} else {
+		return MEM_RO; 
+	}
 }
