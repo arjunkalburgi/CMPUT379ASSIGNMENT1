90cbbdf9024a296ce43222ec266a4a716c84c48f
Arjun Kalburgi <askalburgi@gmail.com>
2017-02-01 12:35:37 -0700

    new implementation

--- findpattern.c
+++ findpattern.c
@@ -4,8 +4,8 @@ int SEGFAULT = 0; //0 = no segfault, 1 = segfault
 
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength, struct patmatch *locations, unsigned int loclength) {
 	// get access to memory (system call?) 
-	char *start = (char *) 0x00000000; 
-	char *end = (char *) 0xFFFFFFFF;
+	u_int32 page_start = 0x00000000; 
+	u_int32 page_end = 0xFFFFFFFF;
 
 	// handler 
 	struct sigaction act;
@@ -14,41 +14,46 @@ unsigned int findpattern (unsigned char *pattern, unsigned int patlength, struct
 
 	// variables 
 	unsigned int patsfound = 0; 
-	char * memory_index;
-	//  = 0x00000000;
-	// char x = *addr;
+	u_int32 *page; 
 
-	for (memory_index=start; memory_index<=end; memory_index++) {
-		char x = *memory_index; 
+	// cycle through pages
+	for (page=page_start; page<page_end; page+=getpagesize()) {
+		char x = *page; 
 		//signal handler
 		if (SEGFAULT == 1) {
-			memory_index = memory_index + end/getpagesize() - 1;
 			SEGFAULT = 0; 
-			continue;
+			continue; // skip to next page
 		}
-		
-		char * pattern_index; 
-		for (pattern_index=0; pattern_index<=(char *)patlength; pattern_index++) {
-			if (pattern[(int)pattern_index] == *memory_index) { // the item at pattern_index is the same as the item at memory_index
 
-				if ((int)pattern_index == patlength) { // we are at the last item in the pattern
-					printf("PAT found\n");
-					if (patsfound < loclength) { // we are under the storage limit
-						patternFound(memory_index, locations, patsfound); // make and store
-					}
-					patsfound++; // increment counter
-				} else { // we are not at the last item in the pattern
-					printf("%s\n", *memory_index);
-					continue; // check the next one
-				}
+		char * memory_index;
+		for (memory_index=page; memory_index<=(page+getpagesize()); memory_index++) {
+			
+			printf("Tellemtellemtellemtellem\n");
+			// char * pattern_index; 
+			// for (pattern_index=0; pattern_index<=(char *)patlength; pattern_index++) {
+			// 	if (pattern[(int)pattern_index] == *memory_index) { // the item at pattern_index is the same as the item at memory_index
 
-			} elseif (&pattern_index != &memory_index) { // we're not at a pattern
-				memory_index = memory_index - pattern_index + 1; // reset memory_index
-				break; // start over
-			}
-			memory_index++;
+			// 		if ((int)pattern_index == patlength) { // we are at the last item in the pattern
+			// 			printf("PAT found\n");
+			// 			if (patsfound < loclength) { // we are under the storage limit
+			// 				patternFound(memory_index, locations, patsfound); // make and store
+			// 			}
+			// 			patsfound++; // increment counter
+			// 		} else { // we are not at the last item in the pattern
+			// 			printf("%s\n", *memory_index);
+			// 			continue; // check the next one
+			// 		}
+
+			// 	} elseif (&pattern_index != &memory_index) { // we're not at a pattern
+			// 		memory_index = memory_index - pattern_index + 1; // reset memory_index
+			// 		break; // start over
+			// 	}
+			// 	memory_index++;
+			// }
 		}
 	}
+
+	
 	
 	return patsfound;
 }
