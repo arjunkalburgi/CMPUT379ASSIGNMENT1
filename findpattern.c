8dab93563a400241769baaabec37175cbf47ed86
Arjun Kalburgi <askalburgi@gmail.com>
2017-02-04 23:12:52 -0700

    new findpattern

--- findpattern.c
+++ findpattern.c
@@ -1,104 +1,87 @@
 #include "findpattern.h"
 
-int SEGFAULT = 0; //0 = no segfault, 1 = segfault
-jmp_buf env;
+sigjmp_buf env; //env=0,no-segfault; env=1,segfault
 
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength, struct patmatch *locations, unsigned int loclength) {
-	// get access to memory (system call?)
-	unsigned int mem_start = 0x00000000;
-	unsigned int mem_end = 0xFFFFFFFF;
+        // get access to memory (system call?)
+        unsigned int mem_start = 0x00000000;
+        unsigned int mem_end = 0x0000FFFF;
 
-	// handler
-	struct sigaction act;
-	act.sa_handler = test;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGSEGV, &act, 0);
+        // handler
+        struct sigaction act;
+        act.sa_handler = test;
+        sigemptyset(&act.sa_mask);
+        sigaction(SIGSEGV, &act, 0);
 
-	// variables
-	unsigned int patsfound = 0;
-	unsigned int page;
+        // variables
+        unsigned int patsfound = 0;
+        unsigned int page;
 
-	printf("getpagesize() %d\n", getpagesize());
+        // cycle through pages
+        for (page = mem_start; page < mem_end; page+=getpagesize()) {
+                unsigned int memory_index;
 
-	// cycle through pages
-	for (page = mem_start; page < mem_end; page+=getpagesize()) {
+                //signal handler
+                if (sigsetjmp(env,0) == 1) {
+                        printf("SEGFAULT at Page Num: %d\n", page);
+                        continue; // skip to next page
+                } else {
+                        memory_index = page;
+                        //printf("no read SEGFAULT\n");
+                }
 
-		sigsetjmp(env,1);
-		printf("start %d\n", page);
-		//signal handler
-		if (SEGFAULT == 1) {
-			printf("inside - SEGFAULT %d - Page Num %d\n", SEGFAULT, page);
-			SEGFAULT = 0;
-			continue; // skip to next page
-		}
-		printf("outsidebef - SEGFAULT %d - Page Num %d\n", SEGFAULT, page);
-		char x = page;
-		printf("outsideaft - SEGFAULT %d - Page Num %d\n", SEGFAULT, page);
+                for (memory_index=page; memory_index<=(page+getpagesize()); memory_index++) { 
+                        unsigned int pattern_index;
+                        for (pattern_index=0; pattern_index<=patlength; pattern_index++) {
+                                if (pattern[pattern_index] == memory_index) { // the item at pattern_index is the same as the item at memory_index                                          
 
-		unsigned int memory_index;
-		for (memory_index=page; memory_index<=(page+getpagesize()); memory_index++) {
-			unsigned int pattern_index;
-			for (pattern_index=0; pattern_index<=patlength; pattern_index++) {
-				if (pattern[pattern_index] == memory_index) { // the item at pattern_index is the same as the item at memory_index
+                                        if (pattern_index == patlength) { // we are at the last item in the pattern
+                                                printf("PAT found\n");
+                                                if (patsfound < loclength) { // we are under the storage limit
+                                                        patternFound(memory_index, locations, patsfound); // make and store
+                                                }
+                                                patsfound++; // increment counter
+                                        } else { // we are not at the last item in the pattern
+                                                printf("%u\n", memory_index);
+                                                continue; // check the next one
+                                        }
 
-					if (pattern_index == patlength) { // we are at the last item in the pattern
-						printf("PAT found\n");
-						if (patsfound < loclength) { // we are under the storage limit
-							patternFound(memory_index, locations, patsfound); // make and store
-						}
-						patsfound++; // increment counter
-					} else { // we are not at the last item in the pattern
-						printf("%u\n", memory_index);
-						continue; // check the next one
-					}
+                                } else if (&pattern_index != &memory_index) { // we're not at a pattern
+                                        memory_index = memory_index - pattern_index + 1; // reset memory_index
+                                        break; // start over
+                                }
+                                memory_index++;
+                        }
+                }
+        }
 
-				} else if (&pattern_index != &memory_index) { // we're not at a pattern
-					memory_index = memory_index - pattern_index + 1; // reset memory_index
-					break; // start over
-				}
-				memory_index++;
-			}
-		}
-	}
-
-	return patsfound;
+        return patsfound;
 }
 
 void test (int sig) {
-	printf("SEGFAULT found\n");
-	printf("I am here\n");
-	SEGFAULT = 1;
-	siglongjmp(env, 1);
-	return;
+        printf("SEGFAULT found - %d\n", sig);
+        siglongjmp(env, 1);
+        return;
 }
 
 void patternFound(unsigned int memory_index, struct patmatch *locations, unsigned int patsfound) {
-	// make patmatch
-	struct patmatch temp;
-	temp.location = (unsigned int) memory_index;
-	temp.mode = memType(memory_index);
+        // make patmatch
+        struct patmatch temp;
+        temp.location = (unsigned int) memory_index;
+        temp.mode = memType(memory_index);
 
-	locations[patsfound*3] = temp;
+        locations[patsfound*3] = temp;
 }
 
 unsigned int memType(unsigned int memory_index) {
-	unsigned int type = MEM_RW;
-
-	//save sess
-	jmp_buf env;
-	int i;
-	i = setjmp(env);
-
-	// test write
-	memory_index = '1';
-	//signal handler
-	if (SEGFAULT == 1) {
-		type = MEM_RO;
-		SEGFAULT = 0;
-	}
-
-	//return sess
-	longjmp(env, 1);
-
-	return type;
+        // if write segfault
+        if (sigsetjmp(env,0) == 1) {
+                //printf("Write SEGFAULT at %d\n", memory_index);
+                return MEM_RO;
+        } else {
+                // test write
+                memory_index = '1';
+                //printf("No Write SEGFAULT at %d\n", memory_index);
+                return MEM_RW;
+        }
 }
