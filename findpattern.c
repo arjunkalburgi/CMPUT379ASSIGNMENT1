8ca184ae8bc37c148a22e37ac34d9a9deb77bdd5
Arjun Kalburgi <askalburgi@gmail.com>
2017-02-06 02:11:00 -0700

    restructure of findpattern

--- findpattern.c
+++ findpattern.c
@@ -28,40 +28,35 @@ unsigned int findpattern (unsigned char *pattern, unsigned int patlength, struct
                         //printf("no read SEGFAULT\n");
                 }
 
+                unsigned int pattern_index = 0; 
                 for (memory_index=page; memory_index<=(page+getpagesize()); memory_index++) { 
-                        unsigned int pattern_index;
-                        for (pattern_index=0; pattern_index<=patlength; pattern_index++) {
-                                if (pattern[pattern_index] == (char *)(memory_index+pattern_index)) { // the item at pattern_index is the same as the item at memory_index                                          
-					printf("%c %c %c %c \n", pattern[pattern_index], pattern[pattern_index+1], pattern[pattern_index+2], pattern[pattern_index+3]); 
-					printf("%c %c %c %c \n", (memory_index+pattern_index), (memory_index+pattern_index+1), (memory_index+pattern_index+2), (memory_index+pattern_index+3));
-                                        if (pattern_index == patlength) { // we are at the last item in the pattern
-                                                printf("PAT found\n");
-                                                if (patsfound < loclength) { // we are under the storage limit
-							unsigned int temp_mem = memory_index;         
-							// make patmatch
-						       	locations[patsfound].location = (unsigned int) memory_index;
-        
-						        if (sigsetjmp(env,0) == 1) {
-						                //printf("Write SEGFAULT at %d\n", memory_index);
-						                locations[patsfound].mode = MEM_RO;
-						        } else {
-						                // test write
-						                memory_index = '1';
-						                //printf("No Write SEGFAULT at %d\n", memory_index);
-						                 locations[patsfound].mode = MEM_RW;
-						        }
-	
-							memory_index = temp_mem; 
-                                                }
-                                                patsfound++; // increment counter
-						memory_index += patlength - 1; //move forward so it doesn't go over these again
-                                        } else { // we are not at the last item in the pattern
-                                                printf("possible pattern @ %u\n", memory_index);
-                                                continue; // check the next one
-                                        }
+                        unsigned int * memory_ptr = memory_index; 
+                        if (*memory_ptr == pattern[pattern_index]) {
+                                pattern_index++; 
+                                printf("on to something: %c\n", *((char *)memory_ptr));
+                                if (pattern_index == patlength) {
+                                        printf("PAT found\n");
+                                        if (patsfound < loclength) { // we are under the storage limit
+                                                unsigned int temp_mem = memory_index;         
+                                                // make patmatch
+                                                locations[patsfound].location = memory_index;
 
-                                } else if (pattern_index>0 && pattern[pattern_index] != (char *)(memory_index+pattern_index)) { // we're not at a pattern, but we thought we were
-                                        break; // start over
+                                                if (sigsetjmp(env,0) == 1) {
+                                                        //printf("Write SEGFAULT at %d\n", memory_index);
+                                                        locations[patsfound].mode = MEM_RO;
+                                                } else {
+                                                        // test write
+                                                        memory_index = '1';
+                                                        //printf("No Write SEGFAULT at %d\n", memory_index);
+                                                        locations[patsfound].mode = MEM_RW;
+                                                }
+
+                                                memory_index = temp_mem; 
+                                        }
+                                        patsfound++; // increment counter
+                                } else {
+                                        memory_index = memory_index - pattern_index +1; 
+                                        pattern_index = 0; 
                                 }
                         }
                 }
